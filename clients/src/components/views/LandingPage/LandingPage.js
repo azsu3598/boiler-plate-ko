c5c64b31ad6ca94b4262cc7bd63abc2851ccc5ba
azsu3598 <azsu@naver.com>
2023-05-27 22:45:18 +0900

    로그인 페이지 중 오류 고쳐야함

--- clients/src/components/views/LandingPage/LandingPage.js
+++ clients/src/components/views/LandingPage/LandingPage.js
@@ -4,12 +4,17 @@ import axios from 'axios';
 function LandingPage() {
     useEffect(() => {
         axios
-            .get('api/hello')
+            .get('/api/hello')
             .then(response => console.log(response))
             .catch(error => console.error(error)); // 에러 처리를 추가했습니다.
     }, []);
 
-    return <div>LandingPage 랜딩페이지</div>; // 한글 텍스트 수정
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: "100%", height: "100vh"
+        }}>
+            <h2>시작페이지</h2></div>) // 한글 텍스트 수정
 }
 
 export default LandingPage;
